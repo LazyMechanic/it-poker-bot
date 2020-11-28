b613c5f4827192c4b0827f9e5b81efe7b9270660
LazyMechanic <AsharnRus@gmail.com>
2020-11-29 00:14:09 +0300

    Add teloxide

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod settings;
 
 use settings::Settings;
+use teloxide::prelude::*;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -12,17 +13,25 @@ async fn run() -> anyhow::Result<()> {
     let settings = Settings::new()?;
     println!("{:?}", settings);
 
+    let bot = Bot::builder()
+        .token(settings.telegram.token)
+        .build();
+
+    teloxide::repl(bot, |message| async move {
+        message.answer_dice().send().await?;
+        ResponseResult::<()>::Ok(())
+    }).await;
+
     Ok(())
 }
 
 fn init_logger() {
     let log_filters = std::env::var("RUST_LOG").unwrap_or_default();
 
-    env_logger::Builder::new()
+    pretty_env_logger::formatted_builder()
         .parse_filters(&log_filters)
         .format(|formatter, record| {
             use std::io::Write;
-
             writeln!(
                 formatter,
                 "{} [{}] - {}",
